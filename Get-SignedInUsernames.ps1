141ef2fce8ff1bc6aab9d36a52ae90bf60d8a7d9
Jared Harber <35547112+JedHarb@users.noreply.github.com>
2023-11-27 15:22:53 -0600

    Update Get-SignedInUsernames.ps1

--- Get-SignedInUsernames.ps1
+++ Get-SignedInUsernames.ps1
@@ -2,13 +2,14 @@
 foreach ($Computer in $Computers) {
 	if (Test-Connection -BufferSize 32 -Count 1 -ComputerName $Computer -Quiet) {
 		Write-Host "$Computer is online." -ForegroundColor 'Green'
-		$SignInCheck = quser /Server:"$Computer" 2>$null # 2>$null supresses error output
+		$SignInCheck = quser.exe /Server:"$Computer" 2>$null # 2>$null supresses error output
 		if ($SignInCheck) {
 			$UserNames = $SignInCheck[1..($SignInCheck.count -1)] | foreach {
 				$_ -replace '^ (\S+) .*','$1' -replace 'username' # Return just the signed in usernames.
 			}
 			# Now you have an array of signed in $UserNames on $Computer
-			# Do something with them here before the next computer loop runs
+			# Do something with them here before the next computer loop runs, for example...
+   			"Signed in users:`n$UserNames"
 		}
 		else {
 			Write-Host "No users are signed into $Computer." -ForegroundColor 'Yellow'
